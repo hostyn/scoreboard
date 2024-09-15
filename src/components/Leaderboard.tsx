e451a88d3bae584be5232cf6c917d02ed29c9664
hostyn <ruben@martinezhostyn.com>
2024-09-15 19:23:46 +0200

    feat: :recycle: Refactor Scoreboard

---
+++ src/components/Leaderboard.tsx
@@ -0,0 +1,46 @@
+import { $scoreboard } from "@/stores/scoreboard";
+import { useStore } from "@nanostores/preact";
+
+export default function Leaderboard() {
+  const scoreboard = useStore($scoreboard);
+
+  const topPlayers = scoreboard.players.sort((a, b) =>
+    a.totalPoints <= b.totalPoints ? 1 : -1
+  );
+
+  return (
+    <div className="grid grid-cols-3 justify-items-center items-end">
+      {topPlayers[1] && (
+        <div className="flex flex-col items-center gap-2">
+          <span className="text-5xl">🥈</span>
+          <span className="font-bold text-gray-300">{topPlayers[1].name}</span>
+          <span className="pb-4 bg-gray-300/15 text-slate-300 w-52 text-center">
+            {topPlayers[1].totalPoints} pts.
+          </span>
+        </div>
+      )}
+      {topPlayers[0] && (
+        <div className="flex flex-col items-center gap-2">
+          <span className="text-5xl">🥇</span>
+          <span className="font-bold text-yellow-300">
+            {topPlayers[0].name}
+          </span>
+          <span className="pb-8 bg-yellow-300/15 text-slate-300 w-52 text-center">
+            {topPlayers[0].totalPoints} pts.
+          </span>
+        </div>
+      )}
+      {topPlayers[2] && (
+        <div className="flex flex-col items-center gap-2">
+          <span className="text-5xl">🥉</span>
+          <span className="font-bold text-yellow-600">
+            {topPlayers[2].name}
+          </span>
+          <span className="bg-yellow-600/15 text-slate-300 w-52 text-center">
+            {topPlayers[2].totalPoints} pts.
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,91 +1,63 @@
+import { $scoreboard, addPlayer } from "@/stores/scoreboard";
 import { useStore } from "@nanostores/preact";
-import { $scoreboard, addPlayer, addRound } from "../stores/scoreboard";
 
-export const Component = () => {
+export default function Scoreboard() {
   const scoreboard = useStore($scoreboard);
-
-  const topPlayers = scoreboard.players.sort((a, b) =>
-    a.totalPoints > b.totalPoints ? 1 : -1
-  );
 
   return (
     <div>
-      <h1>Component</h1>
-      <button
-        onClick={() => {
-          addPlayer("player4");
-        }}
-      >
-        Add players
-      </button>
-
-      <button
-        onClick={() => {
-          addRound([20, 15, 10, 10, 10]);
-        }}
-      >
-        Add round
-      </button>
-
-      <div className="grid grid-cols-3 justify-items-center items-end my-16">
-        {topPlayers[1] && (
-          <div className="flex flex-col items-center mb-4">
-            <span className="text-7xl">🥈</span>
-            <span>{topPlayers[1].name}</span>
-            <span>{topPlayers[1].totalPoints} pts.</span>
-          </div>
-        )}
-        {topPlayers[0] && (
-          <div className="flex flex-col items-center mb-8">
-            <span className="text-7xl">🥇</span>
-            <span>{topPlayers[0].name}</span>
-            <span>{topPlayers[0].totalPoints} pts.</span>
-          </div>
-        )}
-        {topPlayers[2] && (
-          <div className="flex flex-col items-center">
-            <span className="text-7xl">🥉</span>
-            <span>{topPlayers[2].name}</span>
-            <span>{topPlayers[2].totalPoints} pts.</span>
-          </div>
-        )}
-      </div>
-
       <table>
-        <tr>
-          <th></th>
-          <th className="text-left">Player</th>
-          {scoreboard.rounds.map((_round, index) => (
-            <th key={index}>{index + 1}.</th>
-          ))}
-          <th>Total</th>
-        </tr>
-        {scoreboard.players
-          .sort((a, b) => (a.totalPoints > b.totalPoints ? 1 : -1))
-          .map((player, index) => (
-            <tr key={player.index}>
-              <td
-                class={`min-w-6 text-xl font-bold ${
-                  (index === 0 && "text-yellow-300") ||
-                  (index === 1 && "text-gray-300") ||
-                  (index === 2 && "text-yellow-600") ||
-                  "text-slate-600"
-                }`}
-              >
-                {index + 1}
-              </td>
-              <td className="w-full">{player.name}</td>
-              {scoreboard.rounds.map((round, roundIndex) => (
-                <td key={roundIndex} className="min-w-10">
-                  {round[player.index]}
+        <thead>
+          <tr>
+            <th></th>
+            <th className="text-left">Player</th>
+            {scoreboard.rounds.map((_round, index) => (
+              <th key={index} className="text-left">
+                {index + 1}.
+              </th>
+            ))}
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {scoreboard.players
+            .sort((a, b) => (a.totalPoints < b.totalPoints ? 1 : -1))
+            .map((player, index) => (
+              <tr key={player.index}>
+                <td
+                  class={`min-w-6 text-xl font-bold ${
+                    (index === 0 && "text-yellow-300") ||
+                    (index === 1 && "text-gray-300") ||
+                    (index === 2 && "text-yellow-600") ||
+                    "text-slate-600"
+                  }`}
+                >
+                  {index + 1}
                 </td>
-              ))}
-              <td className="text-center min-w-24">
-                {player.totalPoints} pts.
-              </td>
-            </tr>
-          ))}
+                <td className="w-full">{player.name}</td>
+                {scoreboard.rounds.map((round, roundIndex) => (
+                  <td key={roundIndex} className="min-w-10">
+                    {round[player.index]}
+                  </td>
+                ))}
+                <td className="text-center min-w-24">
+                  {player.totalPoints} pts.
+                </td>
+              </tr>
+            ))}
+        </tbody>
       </table>
+      <button
+        className="mt-12"
+        onClick={() => {
+          addPlayer("Ruben");
+          addPlayer("Sergio");
+          addPlayer("Montiel");
+          addPlayer("Alba");
+        }}
+      >
+        Initialize
+      </button>
     </div>
   );
-};
+}
