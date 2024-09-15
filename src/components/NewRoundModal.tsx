72e9e3c5b5e12c89cc54dda7440215b71691d2ac
hostyn <ruben@martinezhostyn.com>
2024-09-15 19:22:02 +0200

    feat: :sparkles: Add NewRoundModal

---
+++ src/components/NewRoundModal.tsx
@@ -0,0 +1,105 @@
+import {
+  $newRound,
+  addPointsToPlayer,
+  addRound,
+  reset,
+  updatePlayerPoints,
+} from "@/stores/newRound";
+import { $scoreboard } from "@/stores/scoreboard";
+import { useStore } from "@nanostores/preact";
+import { useState } from "preact/hooks";
+
+export default function NewRoundModal() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const scoreboard = useStore($scoreboard);
+  const newRound = useStore($newRound);
+
+  return (
+    <>
+      <button
+        className="bg-primary-300 rounded-sm px-2 py-1"
+        onClick={() => {
+          reset();
+          setModalOpen(true);
+        }}
+      >
+        New round
+      </button>
+      <dialog
+        open={modalOpen}
+        className="absolute top-0 bottom-0 left-0 right-0 p-8 bg-primary-950 text-white z-10 rounded-md"
+      >
+        <form
+          className="w-[800px] m-auto flex flex-col gap-2"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setModalOpen(false);
+            addRound();
+          }}
+        >
+          <h2 className="text-xl font-semibold mb-4">
+            Round {scoreboard.rounds.length}
+          </h2>
+          {newRound.map((playerRound) => (
+            <div key={playerRound.index} className="flex gap-2">
+              <span className="w-full text-gray-300">{playerRound.name}</span>
+
+              {[10, 5, 2, 1].map((points) => (
+                <button
+                  type="button"
+                  onClick={() => addPointsToPlayer(playerRound.index, -points)}
+                  className="bg-red-800 hover:bg-red-700 transition-colors rounded-sm min-w-8 aspect-square text-center text-sm"
+                >
+                  -{points}
+                </button>
+              ))}
+
+              <input
+                className="w-12 bg-transparent text-center font-bold"
+                value={playerRound.points}
+                type="number"
+                placeholder="0"
+                onChange={(e) => {
+                  updatePlayerPoints(
+                    playerRound.index,
+                    e.currentTarget.valueAsNumber
+                  );
+                }}
+              />
+
+              {[1, 2, 5, 10].map((points) => (
+                <button
+                  type="button"
+                  onClick={() => addPointsToPlayer(playerRound.index, points)}
+                  className="bg-green-800 hover:bg-green-700 transition-colors rounded-sm min-w-8 aspect-square text-center text-sm"
+                >
+                  +{points}
+                </button>
+              ))}
+            </div>
+          ))}
+          <div className="flex w-full justify-between mt-4">
+            <button
+              type="button"
+              onClick={() => {
+                setModalOpen(false);
+              }}
+              className="text-red-400 font-bold hover:bg-red-900 hover:text-red-100 px-3 py-1 rounded-sm transition-colors"
+            >
+              Cancel
+            </button>
+            <button className="bg-green-600 hover:bg-green-500 transition-colors rounded-sm px-3 py-1">
+              Add round
+            </button>
+          </div>
+        </form>
+      </dialog>
+      <div
+        className={`absolute w-screen h-screen top-0 bottom-0 left-0 right-0 backdrop-blur-sm bg-primary-400/15 ${
+          modalOpen ? "" : "hidden"
+        }`}
+        onClick={() => setModalOpen(false)}
+      />
+    </>
+  );
+}
---
+++ src/stores/newRound.ts
@@ -0,0 +1,59 @@
+import { atom } from "nanostores";
+import { $scoreboard, addRound as addRoundToScoreboard } from "./scoreboard";
+
+interface PlayerRound {
+  name: string;
+  index: number;
+  points: number | undefined;
+}
+
+export const $newRound = atom<PlayerRound[]>([]);
+
+export const reset = () => {
+  $newRound.set(
+    $scoreboard
+      .get()
+      .players.sort((a, b) => (a.index > b.index ? 1 : -1))
+      .map((player) => ({
+        name: player.name,
+        index: player.index,
+        points: undefined,
+      }))
+  );
+};
+
+export const addPointsToPlayer = (index: number, points: number) => {
+  const newRound = [...$newRound.get()];
+  const playerIndex = newRound.findIndex((player) => player.index === index);
+
+  if (playerIndex === -1) throw new Error("Player not found");
+  if (newRound[index] === undefined) throw new Error("Player not found");
+
+  newRound[index].points = newRound[index]?.points
+    ? newRound[index]?.points + points
+    : points;
+
+  $newRound.set(newRound);
+};
+
+export const updatePlayerPoints = (index: number, points: number) => {
+  const newRound = [...$newRound.get()];
+  const playerIndex = newRound.findIndex((player) => player.index === index);
+
+  if (playerIndex === -1) throw new Error("Player not found");
+  if (newRound[index] === undefined) throw new Error("Player not found");
+
+  newRound[index].points = points;
+  $newRound.set(newRound);
+};
+
+export const addRound = () => {
+  const newRound = $newRound
+    .get()
+    .sort((a, b) => (a.index > b.index ? 1 : -1))
+    .map((player) => player.points ?? 0);
+
+  addRoundToScoreboard(newRound);
+
+  reset();
+};
