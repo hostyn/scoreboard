8693846815e99ed583f422517ab56126cedce70d
hostyn <ruben@martinezhostyn.com>
2024-09-29 19:59:42 +0200

    fix: :lipstick: Improve layout styling

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -1,39 +1,32 @@
 import { $games, selectGame } from "@/stores/games";
 import { getScoreboard } from "@/stores/scoreboard";
 import { useStore } from "@nanostores/react";
-import NewGameModal from "./NewGameModal";
 
 export default function Games() {
   const games = useStore($games);
 
   return (
-    <div>
-      <h1>Games</h1>
-      <div className="flex flex-col gap-4">
-        {games.games
-          .sort((a, b) => (a.updatedAt < b.updatedAt ? 1 : -1))
-          .map((game) => {
-            const gameData = getScoreboard(game.id);
-            if (!gameData) return null;
+    <div className="flex flex-col gap-4">
+      {games.games
+        .sort((a, b) => (a.updatedAt < b.updatedAt ? 1 : -1))
+        .map((game) => {
+          const gameData = getScoreboard(game.id);
+          if (!gameData) return null;
 
-            return (
-              <div className="bg-gray-800">
-                <h2>{gameData.name}</h2>
-                <p>{game.updatedAt.toTimeString()}</p>
-                <button
-                  key={game.id}
-                  onClick={() => {
-                    selectGame(game.id);
-                    window.location.href = "/game";
-                  }}
-                >
-                  play
-                </button>
-              </div>
-            );
-          })}
-      </div>
-      <NewGameModal />
+          return (
+            <button
+              key={game.id}
+              className="bg-surface rounded-[12px] border border-outlineVariant p-4 text-left"
+              onClick={() => {
+                selectGame(game.id);
+                window.location.href = "/game";
+              }}
+            >
+              <h2>{gameData.name}</h2>
+              <p>{game.updatedAt.toTimeString()}</p>
+            </button>
+          );
+        })}
     </div>
   );
 }
