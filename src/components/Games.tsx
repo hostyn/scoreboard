21de23d65a64fd42494894607227c91f97064cc7
hostyn <ruben@martinezhostyn.com>
2024-09-29 20:26:32 +0200

    feat: :lipstick: Add typography styling

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -1,5 +1,6 @@
 import { $games, selectGame } from "@/stores/games";
 import { getScoreboard } from "@/stores/scoreboard";
+import { dateFormatter } from "@/util/formatters";
 import { useStore } from "@nanostores/react";
 
 export default function Games() {
@@ -16,14 +17,35 @@ export default function Games() {
           return (
             <button
               key={game.id}
-              className="bg-surface rounded-[12px] border border-outlineVariant p-4 text-left"
+              className="bg-surface rounded-[12px] border border-outlineVariant p-4 text-left flex justify-between items-center"
               onClick={() => {
                 selectGame(game.id);
                 window.location.href = "/game";
               }}
             >
-              <h2>{gameData.name}</h2>
-              <p>{game.updatedAt.toTimeString()}</p>
+              <div>
+                <h2 className="md-typescale-title-medium">{gameData.name}</h2>
+                <p className="md-typescale-body-medium">
+                  {dateFormatter.format(game.updatedAt)}
+                </p>
+              </div>
+              <div className="flex flex-col text-right">
+                {gameData.players[0] && (
+                  <span className="md-typescale-body-medium text-yellow-300">
+                    1. {gameData.players[0]?.name}
+                  </span>
+                )}
+                {gameData.players[1] && (
+                  <span className="md-typescale-body-medium text-gray-300">
+                    2. {gameData.players[1]?.name}
+                  </span>
+                )}
+                {gameData.players[1] && (
+                  <span className="md-typescale-body-medium text-yellow-600">
+                    3. {gameData.players[1]?.name}
+                  </span>
+                )}
+              </div>
             </button>
           );
         })}
---
+++ src/util/formatters.ts
@@ -0,0 +1,7 @@
+export const dateFormatter = new Intl.DateTimeFormat("es-ES", {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+});
