9dd913b7fe2a1b81a846741591eb20f32799282e
hostyn <ruben@martinezhostyn.com>
2024-10-01 10:15:41 +0200

    feat: :lipstick: Style scoreboard

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,12 +1,9 @@
-import { $scoreboard, addPlayer } from "@/stores/scoreboard";
+import { $scoreboard, type Player } from "@/stores/scoreboard";
 import { useStore } from "@nanostores/react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 
 export default function Scoreboard() {
   const scoreboard = useStore($scoreboard);
-
-  const [addingPlayer, setAddingPlayer] = useState(false);
-  const addPlayerInput = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (!scoreboard) {
@@ -16,92 +13,35 @@ export default function Scoreboard() {
 
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th className="text-left">#</th>
-            <th className="text-left">Player</th>
-            {scoreboard?.rounds.map((_round, index) => (
-              <th key={index} className="text-left">
-                {index + 1}.
-              </th>
-            ))}
-            <th>Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          {scoreboard?.players
-            .sort((a, b) => (a.totalPoints < b.totalPoints ? 1 : -1))
-            .map((player, index) => (
-              <tr key={player.index}>
-                <td
-                  className={`min-w-6 text-xl font-bold ${
-                    (index === 0 && "text-yellow-300") ||
-                    (index === 1 && "text-gray-300") ||
-                    (index === 2 && "text-yellow-600") ||
-                    "text-slate-600"
-                  }`}
-                >
-                  {index + 1}
-                </td>
-                <td className="w-full">{player.name}</td>
-                {scoreboard.rounds.map((round, roundIndex) => (
-                  <td key={roundIndex} className="min-w-10">
-                    {round[player.index]}
-                  </td>
-                ))}
-                <td className="text-center min-w-24">
-                  {player.totalPoints} pts.
-                </td>
-              </tr>
-            ))}
-          <tr>
-            <td></td>
-            <td className="w-full">
-              {addingPlayer ? (
-                <form
-                  onSubmit={() => {
-                    const playerName = addPlayerInput.current?.value;
-
-                    if (playerName) {
-                      addPlayer(playerName);
-                    }
-
-                    setAddingPlayer(false);
-                  }}
-                >
-                  <input
-                    type="text"
-                    placeholder="Player name"
-                    className="bg-transparent"
-                    ref={addPlayerInput}
-                    onBlur={() => {
-                      const playerName = addPlayerInput.current?.value;
-
-                      if (playerName) {
-                        addPlayer(playerName);
-                      }
-
-                      setAddingPlayer(false);
-                    }}
-                  />
-                </form>
-              ) : (
-                <button
-                  onClick={() => {
-                    setAddingPlayer(true);
-                    setTimeout(() => {
-                      addPlayerInput.current?.focus();
-                    }, 0);
-                  }}
-                >
-                  + Add player
-                </button>
-              )}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      {scoreboard?.players
+        .sort((a, b) => (a.totalPoints < b.totalPoints ? 1 : -1))
+        .map((player, index) => (
+          <Player key={player.name} player={player} position={index + 1} />
+        ))}
     </div>
   );
 }
+
+const Player = ({ player, position }: { player: Player; position: number }) => (
+  <div className="flex items-center justify-between w-full h-14 gap-2">
+    <span
+      className={`md-typescale-title-large size-10 min-w-10 flex items-center justify-center rounded-full ${
+        position === 1
+          ? "bg-gold/35"
+          : position === 2
+          ? "bg-silver/35"
+          : position === 3
+          ? "bg-bronze/35"
+          : "bg-surfaceContainer"
+      }`}
+    >
+      {position}
+    </span>
+    <span className="md-typescale-body-large text-left w-full">
+      {player.name}
+    </span>
+    <span className="md-typescale-title-medium min-w-max">
+      {player.totalPoints} pts.
+    </span>
+  </div>
+);
